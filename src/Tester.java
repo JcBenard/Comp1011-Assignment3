d6c18a6fb2eeffad399863414ee514d6a9eb5f5d
JcBenard <niteburn@hotmail.com>
2015-02-13 12:00:47 +0900

    Finished Tester, program seems to work as intended

--- src/Tester.java
+++ src/Tester.java
@@ -4,14 +4,18 @@
 
 /**
  * @author Jc
- * @version 3 : added the TerrestrialPlanet class and cleaned up a bit of code
+ * @version 6 : Finished Tester, program seems to work as intended
  * Program description test GiantPlanet and TerrestrialPlanet classes and there assocated methods
  */
 public class Tester {
 
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		GiantPlanet giantPlanet = new GiantPlanet("Jupiter", 11.209, 317.8, "Gas");
+		System.out.println(giantPlanet.toString());
+		
+		TerrestrialPlanet terrestrialPlanet = new TerrestrialPlanet("Mercury", 0.382, 0.06, false);
+		System.out.println(terrestrialPlanet.toString());
 
 	}
 
